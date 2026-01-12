48f52a1ef75409c2bcc2c0549905654a5292bc2b
HA <hagerahour@gmail.com>
2026-01-12 11:33:11 +0200

    Updating package.json to use explicit paths

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,8 +10,12 @@ app = FastAPI(
 
 # CORS
 origins = [
-    "http://localhost:5173", # Vite dev server
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
     "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 
 app.add_middleware(
